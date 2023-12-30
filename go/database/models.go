d100b8c29bf76e3526c89a7f342dd8e9d78fbda2
dshehbaj <shehbaj.dhillon@gmail.com>
2023-12-29 16:50:16 -0800

    go: update schema

--- go/database/models.go
+++ go/database/models.go
@@ -5,8 +5,121 @@
 package database
 
 import (
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
 	"time"
 )
+
+type MembershipType string
+
+const (
+	MembershipTypeOWNER  MembershipType = "OWNER"
+	MembershipTypeADMIN  MembershipType = "ADMIN"
+	MembershipTypeMEMBER MembershipType = "MEMBER"
+)
+
+func (e *MembershipType) Scan(src interface{}) error {
+	switch s := src.(type) {
+	case []byte:
+		*e = MembershipType(s)
+	case string:
+		*e = MembershipType(s)
+	default:
+		return fmt.Errorf("unsupported scan type for MembershipType: %T", src)
+	}
+	return nil
+}
+
+type NullMembershipType struct {
+	MembershipType MembershipType
+	Valid          bool // Valid is true if MembershipType is not NULL
+}
+
+// Scan implements the Scanner interface.
+func (ns *NullMembershipType) Scan(value interface{}) error {
+	if value == nil {
+		ns.MembershipType, ns.Valid = "", false
+		return nil
+	}
+	ns.Valid = true
+	return ns.MembershipType.Scan(value)
+}
+
+// Value implements the driver Valuer interface.
+func (ns NullMembershipType) Value() (driver.Value, error) {
+	if !ns.Valid {
+		return nil, nil
+	}
+	return string(ns.MembershipType), nil
+}
+
+type TeamType string
+
+const (
+	TeamTypePERSONAL TeamType = "PERSONAL"
+	TeamTypeTEAM     TeamType = "TEAM"
+)
+
+func (e *TeamType) Scan(src interface{}) error {
+	switch s := src.(type) {
+	case []byte:
+		*e = TeamType(s)
+	case string:
+		*e = TeamType(s)
+	default:
+		return fmt.Errorf("unsupported scan type for TeamType: %T", src)
+	}
+	return nil
+}
+
+type NullTeamType struct {
+	TeamType TeamType
+	Valid    bool // Valid is true if TeamType is not NULL
+}
+
+// Scan implements the Scanner interface.
+func (ns *NullTeamType) Scan(value interface{}) error {
+	if value == nil {
+		ns.TeamType, ns.Valid = "", false
+		return nil
+	}
+	ns.Valid = true
+	return ns.TeamType.Scan(value)
+}
+
+// Value implements the driver Valuer interface.
+func (ns NullTeamType) Value() (driver.Value, error) {
+	if !ns.Valid {
+		return nil, nil
+	}
+	return string(ns.TeamType), nil
+}
+
+type Team struct {
+	ID               int64
+	Slug             string
+	Name             string
+	StripeCustomerID sql.NullString
+	TeamType         TeamType
+	Created          time.Time
+}
+
+type TeamInvite struct {
+	ID           int64
+	Slug         string
+	TeamID       int64
+	InviteeEmail string
+	Created      time.Time
+}
+
+type TeamMembership struct {
+	ID             int64
+	TeamID         int64
+	UserID         int64
+	MembershipType MembershipType
+	Created        time.Time
+}
 
 type Userinfo struct {
 	ID       int64
